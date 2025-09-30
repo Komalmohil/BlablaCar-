2fd1cef4b1d07051c7be7a486a71e22f41c37746
Komal Mohil <komalmohil1@gmail.com>
2025-09-30 14:55:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ notification(io)
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'Views'));
 
